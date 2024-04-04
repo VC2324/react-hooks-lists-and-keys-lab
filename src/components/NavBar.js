ddddafb21c177b0b5ae9c3762da2d875ae5aba39
Victor <victorac2324@gmail.com>
2024-04-03 20:19:28 -0400

    was able to get the nav bar deliverables and some of the projectList have it rendered but maybe not the rigth elements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 function NavBar() {
+  // Take these links, and make an <a> tag for each one.
+  // <a key="home" href="#home">home</a>
+  // <a key={linkName} href={`#${linkName}`}>{linkName}</a>
   const links = ["home", "about", "projects"];
+  const elementsLinks = links.map((e) =>{
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+    return {links}
+    
+  });
+  console.log (elementsLinks)
+
+  return <nav>
+    <a key="home" href ="#home">Home</a>
+    <a key="about" href ="#about">About</a>
+    <a key="projects" href = "#projects">Projects</a>
+  </nav>;
 }
 
 export default NavBar;
